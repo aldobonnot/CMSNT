c4faa6aa7bb1780f02f512ba11f4938d1fd8d35f
navas thierry <thierry.navas@neuf.fr>
2017-04-29 17:48:00 +0200

    Template page d'accueil
    Ici en plus du contenu concernant la page d’accueil, j'y ai intégré un slider avec wowslider pour ses effets que voulez le peintre.

--- zone/text.php
+++ zone/text.php
@@ -1,28 +1,26 @@
 <div class="global">
 <h1 class="h1"><?php echo $titrecontenu;?> </h1>
-
 <div class="inter">
 <div class="anim">
 <div id="wowslider-container1" > 
     <div class="ws_images"> 
-		<ul>
+	<ul>
             <li><a href="oeuvres_dessins.php"><img src="data1/images/slide4AC2.jpg" alt="Dessins" title="Dessins" id="wows1_0" class="imgslide"></a></li>
             <li><a href="oeuvres_serigraphies.php"><img src="data1/images/slide2AC2.jpg" alt="S&eacute;rigraphie" title="S&eacute;rigraphie" id="wows1_1" class="imgslide"></a></li>
-			<li><a href="oeuvres_peintures.php"><img src="data1/images/slide1AC2.jpg" alt="Peintures" title="Peintures" id="wows1_2" class="imgslide"></a></li>
+	    <li><a href="oeuvres_peintures.php"><img src="data1/images/slide1AC2.jpg" alt="Peintures" title="Peintures" id="wows1_2" class="imgslide"></a></li>
             <li><a href="oeuvres_calligraphies.php"><img src="data1/images/slide3AC2.jpg" alt="Calligraphie" title="Calligraphie" id="wows1_3" class="imgslide"></a></li>
-			<li><a href="oeuvres_supports-speciaux.php"><img src="data1/images/slide5AC2.jpg" alt="Supports speciaux" title="Supports speciaux: Meubles " id="wows1_4"  class="imgslide"></a></li>
-			<li><a href="oeuvres_supports-speciaux.php"><img src="data1/images/slide6AC2.jpg" alt="Supports speciaux" title="Supports speciaux: Fresque " id="wows1_5"  class="imgslide"></a></li>
+	    <li><a href="oeuvres_supports-speciaux.php"><img src="data1/images/slide5AC2.jpg" alt="Supports speciaux" title="Supports speciaux: Meubles " id="wows1_4"  class="imgslide"></a></li>
+	    <li><a href="oeuvres_supports-speciaux.php"><img src="data1/images/slide6AC2.jpg" alt="Supports speciaux" title="Supports speciaux: Fresque " id="wows1_5"  class="imgslide"></a></li>
         </ul>
-		
-    </div><div class="txt">
-<?php echo $contenu;?>	
-</div>
+    </div>
+    <div class="txt">
+           <?php echo $contenu;?>	
+     </div>
 </div>
 <script type="text/javascript" src="engine1/wowslider.js" defer></script>
 <script type="text/javascript" src="engine1/script.js" defer></script>
-</div></div>
-
-
+</div>
+</div>
 </div>
 <div class="clear"></div>
 <!--Page d'accueil!-->
