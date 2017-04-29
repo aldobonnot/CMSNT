4360b64fa285f80a2f8120d456993edf1391e0b9
navas thierry <thierry.navas@neuf.fr>
2017-04-29 17:30:30 +0200

    Liste des sous rubriques
    Ces listes de rubriques et sous rubriques vous paraissent inutiles, faites comme bon vous semble. mais il faudra revoir tous le coeur du système qui les affiches automatiquement. Si je les ai créées c'est qu'elles un rôle primordiale lequel?

--- zone/listessrubrique.php
+++ zone/listessrubrique.php
@@ -1,5 +1,4 @@
-<div class="global">
-<h1 class="h1"><?php echo $titrecontenu;?> </h1>
+<div class="global"><h1 class="h1"><?php echo $titrecontenu;?> </h1>
 <div class="txt"><?php echo $contenu;?> <br>
 <table class="tabLR">
 <?php 
