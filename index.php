12049eacec323123abed9429a594d6daad9c9775
navas thierry <thierry.navas@neuf.fr>
2017-04-29 11:37:18 +0200

    page accueil du site index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php session_start();
+require_once('int/cxCMS.php');//connexion BDD
+require_once('int/Ahead.php');//entete de la page
+require_once('int/Bheader.php');//header
+require_once('int/Cnav.php');//menu
+require_once('int/Dtemplate.php');//template
+require_once('int/Efooter.php');//footer et fin de page
