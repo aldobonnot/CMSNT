8e3a7df542346e957b991b53b630fee39b78175d
navas thierry <thierry.navas@neuf.fr>
2017-04-29 15:05:53 +0200

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php session_start();
-header( 'content-type: text/html; charset=utf-8' );
-require_once('int/cxCMS.php');//connexion BDD
-require_once('int/Ahead.php');//entete de la page
-require_once('int/Bheader.php');//header
-require_once('int/Cnav.php');//menu
-require_once('int/Dtemplate.php');//template
-require_once('int/Efooter.php');//footer et fin de page
