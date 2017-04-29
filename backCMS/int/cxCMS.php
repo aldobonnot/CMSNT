7cb0b7d388db3d030cd1f7642a46ca4465b47f52
navas thierry <thierry.navas@neuf.fr>
2017-04-29 18:18:33 +0200

    Connexion

--- backCMS/int/cxCMS.php
+++ backCMS/int/cxCMS.php
@@ -1,5 +1,5 @@
 <?php
-//===== connexion
+//===== connexion==
 $pdo= new PDO('mysql:dbname=tksom_bd;host=localhost', 'root', '',
 array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8' ));
 
