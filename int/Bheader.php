11d3824afdf637aa28e0fff27755160485201727
navas thierry <thierry.navas@neuf.fr>
2017-05-03 15:06:40 +0200

    Entete du site

--- int/Bheader.php
+++ int/Bheader.php
@@ -15,6 +15,6 @@
   js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.8";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<header>
+<header><!--Entete du site -->
 <div class="ent"><a href="<?= URL ;?>" class="tts" title="Accueil TKSom"><?= NOM_ENTREPRISE;?></a></div>
 </header>
